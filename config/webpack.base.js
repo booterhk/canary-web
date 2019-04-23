33bc51d05fd2cd9dfeb527e4138fe26d0f07acec
何凯 <F160980@efivestar.com>
2019-04-23 21:34:03 +0800

    webpack 实践

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,6 +1,10 @@
 const {resolve} = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const extractTextPlugin = require('extract-text-webpack-plugin')
 
+const website = {
+  publicPath: 'http://localhost:8080'
+}
 module.exports = {
   entry: {
     main: './src/main.js',
@@ -8,17 +12,30 @@ module.exports = {
   },
   output: {
     path: resolve(__dirname, '../dist'),
-    filename: '[name].js'
+    filename: '[name].js',
+    publicPath: '/'
   },
   module: {
-    rules: [{
-      test: /\.css$/,
-      use: [
-        {loader: 'style-loader'},
-        {loader: 'css-loader'}
-      ]
-    },
-  ]
+    rules: [
+      {
+        test: /\.css$/,
+        use: extractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg)/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 500
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -29,5 +46,6 @@ module.exports = {
         removeAttributeQuotes: true
       }
     }),
+    new extractTextPlugin('css/index.css')
   ]
 }
